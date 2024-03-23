3dfde6935e64fcf6f4f16749e8177962c74cc80d
Brandon <bclai27@gmail.com>
2024-03-23 18:40:33 -0400

    Make input fields required

--- frontend/src/Input.js
+++ frontend/src/Input.js
@@ -18,43 +18,43 @@ function Input() {
           <Col xs={{span: 1}}>
             <Form.Group className="mb-3" controlId="inputFormX">
                 <Form.Label>x0</Form.Label>
-                <Form.Control type="number" placeholder="Value" />
+                <Form.Control type="number" placeholder="Value" required/>
               </Form.Group>
           </Col>
           <Col xs={{span: 1}}>
             <Form.Group className="mb-3" controlId="inputFormY">
                 <Form.Label>y0</Form.Label>
-                <Form.Control type="number" placeholder="Value" />
+                <Form.Control type="number" placeholder="Value" required/>
               </Form.Group>
           </Col>
           <Col xs={{span: 1}}>
             <Form.Group className="mb-3" controlId="inputFormZ">
                 <Form.Label>z0</Form.Label>
-                <Form.Control type="number" placeholder="Value" />
+                <Form.Control type="number" placeholder="Value" required/>
               </Form.Group>
           </Col>
           <Col xs={{span: 1}}>
             <Form.Group className="mb-3" controlId="inputFormSigma">
                 <Form.Label>Sigma</Form.Label>
-                <Form.Control type="number" placeholder="Value" />
+                <Form.Control type="number" placeholder="Value" required/>
               </Form.Group>
           </Col>
           <Col xs={{span: 1}}>
             <Form.Group className="mb-3" controlId="inputFormRho">
                 <Form.Label>Rho</Form.Label>
-                <Form.Control type="number" placeholder="Value" />
+                <Form.Control type="number" placeholder="Value" required/>
               </Form.Group>
           </Col>
           <Col xs={{span: 1}}>
             <Form.Group className="mb-3" controlId="inputFormBeta">
                 <Form.Label>Beta</Form.Label>
-                <Form.Control type="number" placeholder="Value" />
+                <Form.Control type="number" placeholder="Value" required/>
               </Form.Group>
           </Col>
           <Col xs={{span: 1}}>
             <Form.Group className="mb-3" controlId="inputFormDelta">
                 <Form.Label>Delta t</Form.Label>
-                <Form.Control type="number" placeholder="Value" />
+                <Form.Control type="number" placeholder="Value" required/>
               </Form.Group>
           </Col>
         </Row>
